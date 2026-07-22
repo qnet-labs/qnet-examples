140fe8bc4a25fddcb076c06107b7da466d98a7e5
Vitalie <vitalie@5e:f6:3c:87:72:8a.home>
2026-07-22 10:27:53 -0400

    Adding the build foledr

---
+++ docs/assets/js/a7456010.385b6d3e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkqnet_core_docs=self.webpackChunkqnet_core_docs||[]).push([[235],{8552(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
